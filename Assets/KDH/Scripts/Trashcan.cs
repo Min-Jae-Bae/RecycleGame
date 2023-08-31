6dcb1d566154d943d222f8839e14d7f401148cbe
xalswox <qoalswo123@icloud.com>
2023-08-31 18:18:37 +0900

    Add: 카메라 쉐이킹

--- Assets/KDH/Scripts/Trashcan.cs
+++ Assets/KDH/Scripts/Trashcan.cs
@@ -1,3 +1,4 @@
+using EZCameraShake;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,21 +7,20 @@ public class Trashcan : MonoBehaviour
 {
     //어떤 종류의 쓰레기통인지.
     public RecycleObject.ObjectType TrashType;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        //쓰레기가 들어올때 
+        //쓰레기가 들어올때
         RecycleObject obj = other.GetComponent<RecycleObject>();
         obj = obj.enabled ? obj : null;
         if (!obj)
@@ -29,11 +29,13 @@ public class Trashcan : MonoBehaviour
         if (TrashType == obj.myType)
         {
             Debug.Log("너 맞음");
-            ScoreManager.instance.SCORE+= obj.add_score;
+            ScoreManager.instance.SCORE += obj.add_score;
         }
-        else {
+        else
+        {
             Debug.Log("너 틀림");
             ScoreManager.instance.SCORE -= obj.less_score;
+            CameraShaker.Instance.ShakeOnce(4f, 4f, 1f, 1f);
         }
         Destroy(other.gameObject);
     }
